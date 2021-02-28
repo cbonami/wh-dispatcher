2712332c98c768e4596d1a6608df517bd9d1b605
cbonami <cbonami@gmail.com>
2021-02-28 18:55:45 +0000

    improved grafana script

--- grafana/start-grafana.sh
+++ grafana/start-grafana.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
-export GF_PATHS_CONFIG="/etc/grafana/grafana.ini"
-export GF_PATHS_DATA="/var/lib/grafana"
-export GF_PATHS_HOME="/usr/share/grafana"
-export GF_PATHS_LOGS="/var/log/grafana"
-export GF_PATHS_PLUGINS="/var/lib/grafana/plugins"
-export GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
-exec grafana-server --homepath="$GF_PATHS_HOME" --config="$GF_PATHS_CONFIG"
+GF_PATHS_CONFIG="/etc/grafana/grafana.ini"
+GF_PATHS_DATA="/var/lib/grafana"
+GF_PATHS_HOME="/usr/share/grafana"
+GF_PATHS_LOGS="/var/log/grafana"
+GF_PATHS_PLUGINS="/var/lib/grafana/plugins"
+GF_PATHS_PROVISIONING="/etc/grafana/provisioning"
+GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-piechart-panel
+
+# install plugins
+if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
+  OLDIFS=$IFS
+  IFS=','
+  for plugin in ${GF_INSTALL_PLUGINS}; do
+    IFS=$OLDIFS
+    grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
+  done
+fi
+
+# run grafana server
+exec grafana-server --homepath="$GF_PATHS_HOME" --config="$GF_PATHS_CONFIG" \
+  cfg:default.log.mode="console"                            \
+  cfg:default.paths.data="$GF_PATHS_DATA"                   \
+  cfg:default.paths.logs="$GF_PATHS_LOGS"                   \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"             \
+  cfg:default.paths.provisioning="$GF_PATHS_PROVISIONING"
